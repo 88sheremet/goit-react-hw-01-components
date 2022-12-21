e48d867588d7c4572b87a332e3779fe8c6148784
Oleksandr Sheremet <88sheremet@gmail.com>
2022-12-21 21:07:14 +0200

    change

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,35 +1,35 @@
-import PropTypes from "prop-types"
-import css from "../TransactionHistory/TransactionHistory.module.css"
+import PropTypes from 'prop-types';
+import css from '../TransactionHistory/TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
-    return (
-      <table>
-        <thead>
-          <tr >
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
-  
-        <tbody>
-          {items.map(({ id, type, amount, currency }) => {
-            return (
-              <tr key={id} >
-                <td>{type}</td>
-                <td>{amount}</td>
-                <td>{currency}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-  };
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 TransactionHistory.prototype = {
-    key: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    amount: PropTypes.string.isRequired,
-    currency: PropTypes.string.isRequired, 
-}
+  key: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
