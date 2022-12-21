f280c5dd9d0ced1bea542aa726f6150404964dba
Oleksandr Sheremet <88sheremet@gmail.com>
2022-12-21 20:58:58 +0200

    add

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,16 +1,17 @@
 import PropTypes from "prop-types"
+import css from "../FriendList/FriendList.module.css"
 
 export const FriendList = ({ friends }) => {
   
 return (
-    <div className="">
-      <ul className="">
+    <div className={css.friends}>
+      <ul className={css.friends__list}>
         {friends.map(({ avatar, name, isOnline, id }) => (
-          <li className="" key={id}>
-            <span className="">
+          <li className={css.friends__item} key={id}>
+            <span className={`${css.status} ${css[isOnline]}`}>
               {isOnline}
             </span>
-            <img className="" src={avatar} alt={name} />
+            <img className={css.avatar} src={avatar} alt={name} width="48" />
             <p className="">{name}</p>
           </li>
         ))}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types"
+import css from "../Statistics/Statistics.module.css"
 
 export const Statistics = ({title,stats}) =>{
-   return <section className="statistics">
-  <h2 className="title">{title}</h2>
+   return <section className={css.statistics}>
+  <h2 className={css.title}>{title}</h2>
 
-  <ul className="stat-list">
+  <ul className={css.statlist}>
     { stats.map(({id,label,percentage}) => {
-            return (<li className="item" key={id}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}</span>
+            return (<li className={css.item} key={id}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}</span>
           </li>)
         })   
 }
